87befde02d76d09ed5cc8b131715b9402cfec315
Yaron Velner <yaron172@yahoo.com>
2017-06-21 10:47:07 +0300

    before tests

--- KyberReserve.sol
+++ KyberReserve.sol
@@ -404,14 +404,14 @@ contract KyberNetwork {
     function trade( ERC20 source, uint srcAmount,
                     ERC20 dest, address destAddress, uint maxDestAmount,
                     uint minConversionRate,
-                    bool throwOnFailure ) payable returns(bool) {
+                    bool throwOnFailure ) payable returns(uint) {
 
         if( ! validateTradeInput( source, srcAmount ) ) {
             // invalid input
             ErrorReport( tx.origin, 0x86000000, 0 );
             if( msg.value > 0 ) msg.sender.transfer(msg.value);
             if( throwOnFailure ) throw;
-            return false;
+            return 0;
         }
 
         TradeInfo memory tradeInfo = TradeInfo(0,srcAmount,false);
@@ -456,7 +456,7 @@ contract KyberNetwork {
             if( throwOnFailure ) throw;
             if( msg.value > 0 ) msg.sender.transfer(msg.value);
             
-            return false;
+            return 0;
         }
         else {
             ErrorReport( tx.origin, 0, 0 );
@@ -465,7 +465,7 @@ contract KyberNetwork {
             }
             ErrorReport( tx.origin, 0, 0 );
             Trade( msg.sender, source, dest, srcAmount-tradeInfo.remainedSourceAmount, tradeInfo.convertedDestAmount );
-            return true;
+            return tradeInfo.convertedDestAmount;
         }
     }
     
@@ -514,14 +514,116 @@ contract KyberNetwork {
         throw;
     }
     
-    // should be called off chain
-    function getReserves( ) constant returns(address[]) {
-        address[] result;
-        for( uint i = 0 ; i < reserves.length ; i++ ) {
-            result.push(reserves[i]);
-        }
-        
-        return result;
+    // should be called off chain with as much gas as needed
+    function getReserves( ) constant returns(KyberReserve[]) {
+        return reserves;
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+contract KyberWallet {
+    address public owner;
+    KyberNetwork public kyberNetwork;
+    ERC20 constant public ETH_TOKEN_ADDRESS = ERC20(0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee);    
+
+    event ErrorReport( address indexed origin, uint error, uint errorInfo );
+    
+    event NewWallet( address indexed owner, address kyberNetwork );
+    function KyberWallet( KyberNetwork _kyberNetwork ) {
+        owner = msg.sender;
+        kyberNetwork = _kyberNetwork;
+        NewWallet( msg.sender, kyberNetwork );
+    }
+    
+    event SetKyberNetwork( address indexed sender, address network );
+    function setKyberNetwork( KyberNetwork network ) {
+        if( msg.sender != owner ) {
+            ErrorReport( msg.sender, 0x8900000, uint(owner) );
+            return;
+        }
+        
+        kyberNetwork = network;
+        ErrorReport( msg.sender, 0, 0 );
+        SetKyberNetwork( msg.sender, network );
+    }
+    
+    event IncomingEther( address sender, uint amountInWei );
+    function() payable {
+        IncomingEther( msg.sender, msg.value );
+    }
+    
+    event ConvertAndCall( address indexed sender, address destination, uint destAmount );
+    function convertAndCall( ERC20 srcToken, uint srcAmount,
+                             ERC20 destToken, uint maxDestAmount,
+                             uint minRate,
+                             address destination,
+                             bytes   destinationData,
+                             bool throwOnFail,
+                             bool validate ) {
+                                 
+        if( validate ) {
+            if( srcToken == ETH_TOKEN_ADDRESS ) {
+                if( this.balance < srcAmount ) {
+                    // balance < srcAmount
+                    ErrorReport( msg.sender, 0x8a00000, this.balance );
+                    return;
+                }
+            }
+            else {
+                if( srcToken.balanceOf(this) < srcAmount ) {
+                    // msg.value < srcAmount
+                    ErrorReport( msg.sender, 0x8a00001, srcToken.balanceOf(this) );
+                    return;
+                }
+            }
+        }
+
+        uint valueForKyberNetwork = 0;
+        if( srcToken == ETH_TOKEN_ADDRESS ) valueForKyberNetwork = srcAmount;
+        else {
+            srcToken.approve(kyberNetwork, srcAmount);
+        }
+        
+        // do trade
+        uint destAmount = kyberNetwork.trade.value(valueForKyberNetwork)(srcToken, srcAmount, destToken, this, maxDestAmount, minRate, throwOnFail );
+        if( destAmount == 0 ) {
+            // trade failed
+            ErrorReport( msg.sender, 0x8a00002, 0 );
+            return;
+        }
+        
+        // reset allowance
+        if( srcToken != ETH_TOKEN_ADDRESS ) {
+            srcToken.approve(kyberNetwork, 0 );
+        }
+        
+        // call destination
+        uint valueForCall = 0;
+        if( destToken == ETH_TOKEN_ADDRESS ) {
+            valueForCall = destAmount;
+        }
+        else {
+            destToken.approve(destination, destAmount);
+        }
+        
+        if( ! destination.call.value(valueForCall)(destinationData) ) {
+            // call to function
+            ErrorReport( msg.sender, 0x8a00003, 0 );
+            if( throwOnFail ) throw;
+            // this address cannot be trusted
+            destToken.approve(destination, 0);
+            return;
+        }
+        
+        if( destToken != ETH_TOKEN_ADDRESS ) {
+            destToken.approve(destination, 0);
+        }
+        
+        ErrorReport( msg.sender, 0, 0 );
+        ConvertAndCall( msg.sender, destination, destAmount );
+    }
+}
+
+
 
